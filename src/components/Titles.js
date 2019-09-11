267336123d06a914125b9e89fb4e037f7a4b1fa3
ericrkock <ericrkock@gmail.com>
2019-09-11 23:04:40 +0100

    Updates & CSS

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,9 @@ class Titles extends React.Component {
    render() {
       return (
          <div>
-            <div className="weatherbox">
-               <h3>Local Weather App</h3>
+            <div>
+               <h1 className="title-container__title">Weather Finder</h1>
+               <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
             </div>
          </div>
       );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './weather.css';
 
 import ProjectHeader from './components/ProjectHeader';
 import ProjectFooter from './components/ProjectFooter';
@@ -10,28 +12,76 @@ import Form from './components/Form';
 import LocalWeather from './components/LocalWeather';
 import WeatherIcon from './components/WeatherIcon';
 
-const API_KEY = "78da2c1f826a4d1f5015230c348bd398";
+//const API_KEY = "78da2c1f826a4d1f5015230c348bd398";
+
 class App extends React.Component {
-    getWeather = async (event) => {
-        event.preventDefault();
-        const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}&units=metric');
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const API_KEY = "78da2c1f826a4d1f5015230c348bd398";
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric');
         const data = await api_call.json();
-        console.log(data);
+        if (city && country) {
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weater[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter City and Country"
+            });
+        };
     }
 
     render() {
         return (
             <div>
                 <ProjectHeader />
-
-                <Titles />
-                <Form 
-                    getWeather={this.getWeather}
-                />
-                <LocalWeather />
-                <WeatherIcon />
-                
-               <ProjectFooter />
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-4 title-container">
+                                    <Titles />
+                                </div>
+                                <div className="col-xs-8 form-container">
+                                    <Form
+                                        getWeather={this.getWeather}
+                                    />
+                                    <LocalWeather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description={this.state.description}
+                                        error={this.state.error}
+                                    />
+                                    <WeatherIcon />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ProjectFooter />
             </div>
         );
     }
